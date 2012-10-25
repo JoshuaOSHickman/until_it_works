c343a55ded011ccef3b9db1aa8acad6bcc02d1ef
Orion Hickman <orion@socialvibe.com>
2012-10-25 11:15:41 -0700

    fixed length retries

--- lib/until_it_works.rb
+++ lib/until_it_works.rb
@@ -16,3 +16,22 @@ def until_it_works(danger=false, opts = {}, &blk)
     return blkval
   end
 end
+
+def try_for(time, &blk)
+  t = Time.now
+  exceptions = []
+  while true
+    if Time.now > t + time
+      if exceptions.first
+        raise exceptions.first
+       else
+        raise ArgumentError, "Timed out without even being able to try"
+      end
+    end
+    begin
+      return blk.call
+    rescue => e
+      exceptions << e
+    end
+  end
+end
